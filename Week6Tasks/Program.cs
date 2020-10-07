32777a3882fa5a05b94b8ae6fe33f22b46e4cd14
Opilane TTHK <opilane@tthk.ee>
2020-10-07 16:36:51 +0300

    Week6Tasks

---
+++ Week6Tasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week6Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //{random superhero} will fight {random Supervillain} with a {random weapon}*
+
+            string[] superHeroes = new string[5] { "Batman", "Rick Grimes", "Pepe", "Thor", "Spider man" };
+            string[] superVillains = new string[5] { "Rogue Superman", "Riddler", "Joker", "Darth Vader", "T-1000" };
+            string[] weapon = new string[5] { "Colt python", "knife", "battarang", "thin air", "bag of sand" };
+            int i = 0;
+            while (i < superHeroes.Length) 
+            while (i < superVillains.Length) 
+            while (i < weapon.Length) 
+            
+           i++;
+           Random rnd = new Random();
+            Console.WriteLine($"{superHeroes[rnd.Next(0,superHeroes.Length)]} will fight {superVillains[rnd.Next(0,superVillains.Length)]} with {weapon[rnd.Next(0,weapon.Length)]}");
+            
+
+            
+            
+           
+
+        }
+    }
+}
---
+++ WeekTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WeekTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 3
+            Random rnd = new Random();
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(1, 11);
+
+            }
+
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            int min = numbers[0];
+            int max = numbers[0];
+
+            for(int i =0;i<numbers.Length; i++)
+            {
+                if(min > numbers[i])
+                {
+                 min = numbers[i];
+                }
+
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+
+            
+            Console.WriteLine($"Max:{max}");
+            Console.WriteLine($"Min:{min}");
+                
+            
+
+                
+                
+            
+
+        }
+        
+    }
+}
